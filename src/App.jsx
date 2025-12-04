03efbc038c34690290a6fc308445557c9ccd81b3
Estevivanco <esteban@vivanco.se>
2025-12-04 11:14:35 +0100

    todo-list

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import TodoList from './components/todo/TodoList'
 
 function App() {
 
   return (
     <>
     <h1>Git-Lektion</h1>
+    <TodoList/>
     </>
   )
 }
