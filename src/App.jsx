e1e589e16d8719445e508d36df6782f847a00aa6
Milad <miladposhtkohi@gmail.com>
2025-12-04 11:32:32 +0100

    miniräknare

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState } from 'react'
 import './App.css'
+import MiniRäknare from './components/todo/MiniRäknare'
 
 function App() {
 
   return (
     <>
     <h1>Git-Lektion</h1>
+    <MiniRäknare/>
     </>
   )
 }
